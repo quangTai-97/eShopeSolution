1184e79ca236e95a62ead6d0cbb42b8a2f7d435c
quangTai-97 <dqtai97@gmail.com>
2020-07-07 22:13:06 +0700

    Miguration Database

---
+++ eShopSolution.Data/Configurations/AppConfigaration.cs
@@ -0,0 +1,20 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Configurations
+{
+    public class AppConfigaration : IEntityTypeConfiguration<AppConfig>
+    {
+        public void Configure(EntityTypeBuilder<AppConfig> builder)
+        {
+            builder.ToTable("AppConfigs"); //Tạo tên bảng trong database
+            builder.HasKey(x => x.Key);
+
+            builder.Property(x => x.Value).IsRequired(true);
+        }
+    }
+}
--- eShopSolution.Data/Entities/Product.cs
+++ eShopSolution.Data/Entities/Product.cs
@@ -12,7 +12,7 @@ namespace eShopSolution.Data.Entities
         public decimal OriginalPrice { set; get; }
         public int Stock { set; get; }
         public int ViewCount { set; get; }
-        public DateTime DateCreated { set; get; }
+        public DateTime? DateCreated { set; get; }
         public string SeoAlias { set; get; }
 
         public List<ProductInCategory> ProductInCategories { get; set; }
