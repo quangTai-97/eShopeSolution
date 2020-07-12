f19904f3781446818f8d39da0e9c2490c5567bbb
quangTai-97 <dqtai97@gmail.com>
2020-07-12 08:56:08 +0700

    Create structure Application Layer

---
+++ eShopSolution.Application/CommonDtos/PagedViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.CommonDtos
+{
+    public class PagedViewModel<T>
+    {
+        List<T> Items { set; get; }
+
+        public int TotalRecord { set; get; }
+
+    }
+}
